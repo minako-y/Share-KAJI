2b5bc90a56ef84ed855e43c1defdb8e3e924f5f9
minako-y <m.tp2uk@gmail.com>
2021-08-19 05:59:25 +0000

    [Update]メッセージ機能非同期で作成

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,22 @@
 class MessagesController < ApplicationController
-  def show
+
+  def create
+    @task = Task.find(params[:task_id])
+    @message = Message.new(message_params)
+    @message.task_id = @task.id
+    @message.user_id = current_user.id
+    if @message.save
+      @message = Message.new
+    else
+      render 'error'
+    end
   end
+
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body)
+  end
+
 end
