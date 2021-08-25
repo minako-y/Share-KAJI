2e40b31c50892c97952f6d9a15b1c89d304a8673
minako-y <m.tp2uk@gmail.com>
2021-08-25 13:10:42 +0000

    [Design]モンスター周辺のレイアウトを修正、monster-newのスライドが開かないバグを修正

--- app/assets/javascripts/new-monster.js
+++ app/assets/javascripts/new-monster.js
@@ -1,6 +1,6 @@
 /*global $*/
 
-$(function(){
+$(document).on('turbolinks:load', function(){
   $('.monsters-container__title--slide').on('click', function(){
     $('#new-monster-form').slideToggle();
   });
