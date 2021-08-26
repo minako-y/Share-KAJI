d9ed1fa90c18cf53bcb4fcc269a8a4a9353c5cd1
minako-y <m.tp2uk@gmail.com>
2021-08-26 07:02:18 +0000

    [fix]テンプレタスクにて一度タグでソートしたら戻せなくなるバグを修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -102,8 +102,13 @@ class TasksController < ApplicationController
   end
 
   def search_template_task
-    @tag = Tag.find(params[:tag_id])
-    @template_tasks = @tag.template_tasks.where(room_id: session[:room_id]).where.not(genre_id: 7).or(@tag.template_tasks.where(user_id: current_user.id, genre_id: 7))
+    if params[:tag_id].nil?
+      @tag = Tag.new(id: 0)
+      @template_tasks = TemplateTask.where(room_id: session[:room_id]).where.not(genre_id: 7).or(TemplateTask.where(user_id: current_user.id, genre_id: 7))
+    else
+      @tag = Tag.find(params[:tag_id])
+      @template_tasks = @tag.template_tasks.where(room_id: session[:room_id]).where.not(genre_id: 7).or(@tag.template_tasks.where(user_id: current_user.id, genre_id: 7))
+    end
     @tag_list = Tag.joins(:template_tasks).distinct.where(template_tasks: {room_id: session[:room_id]})
                                           .where.not(template_tasks: {genre_id: 7})
                                           .or(Tag.joins(:template_tasks).distinct.where(template_tasks: {user_id: current_user.id, genre_id: 7}))
