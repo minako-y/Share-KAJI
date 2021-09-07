8613d53f4177aa09799ff7c3f1be072ccc2f948b
minako-y <m.tp2uk@gmail.com>
2021-09-07 14:20:06 +0000

    [Design]task一覧のレスポンシブ途中

--- app/assets/javascripts/room-box_open.js
+++ app/assets/javascripts/room-box_open.js
@@ -1,7 +1,7 @@
 /* global $*/
 
 $(document).on('turbolinks:load', function(){
-  $('.main-top__room-unit__head').on('click',function(){
+  $('.main-container__room-unit__head').on('click',function(){
     $('.room-box').slideToggle();
   });
 });
