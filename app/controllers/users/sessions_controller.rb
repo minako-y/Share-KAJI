1d53eae2f2495f8d9da3d876839e7d2fa6bea410
minako-y <m.tp2uk@gmail.com>
2021-09-15 19:50:54 +0000

    [fix]自分以外が作ったルームに入室した場合に通知が機能しないバグを修正

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -26,6 +26,12 @@ class Users::SessionsController < Devise::SessionsController
     log_in room
     user.current_room_id = room.id
     user.save
+    # 中間テーブルに登録済みかを確認し、データがない場合登録する
+    user_room = UserRoom.where(user_id: user.id, room_id: room.id)
+    if user_room.empty?
+      user_room = UserRoom.new(user_id: user.id, room_id: room.id)
+      user_room.save
+    end
     redirect_to user_path(user.id), notice: 'ゲストユーザーとしてログインしました。「タスク管理」よりルームへ入室できます。'
   end
 
