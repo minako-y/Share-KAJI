b8b15b5e8e7d9705890d7dd4fb2326dddc75ab1d
minako-y <m.tp2uk@gmail.com>
2021-10-07 12:53:18 +0000

    [add]monsterモデルの単体テスト

---
+++ spec/models/monsters_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Monsterモデルのテスト', type: :model do
+  describe 'バリデーションのテスト' do
+    subject { monster.valid? }
+
+    let!(:user) { build(:user) }
+    let(:monster) { build(:monster, user_id: user.id) }
+
+    context 'nameカラム' do
+      it '空欄でないこと' do
+        monster.name = ''
+        is_expected.to eq false
+      end
+    end
+    context 'memoカラム' do
+      it '空欄でないこと' do
+        monster.memo = ''
+        is_expected.to eq false
+      end
+    end
+    context 'image_idカラム' do
+      it '空欄でないこと' do
+        monster.image_id = ''
+        is_expected.to eq false
+      end
+    end
+    context 'genre_idカラム' do
+      it 'ジャンルが未選択でないこと' do
+        monster.genre_id = ''
+        is_expected.to eq false
+      end
+    end
+  end
+end
--- spec/models/rooms_spec.rb
+++ spec/models/rooms_spec.rb
@@ -2,8 +2,8 @@ RSpec.describe 'Roomモデルのテスト', type: :model do
   describe 'バリデーションのテスト' do
     subject { room.valid? }
 
-    let(:other_room) { create(:room) }
-    let!(:room) { build(:room) }
+    let!(:other_room) { create(:room) }
+    let(:room) { build(:room) }
 
     context 'nameカラム' do
       it '空欄でないこと' do
@@ -37,14 +37,6 @@ RSpec.describe 'Roomモデルのテスト', type: :model do
       it '6文字以上であること: 5文字は×' do
         room.password = Faker::Lorem.characters(number: 5)
         is_expected.to eq false
-      end
-    end
-  end
-
-  describe 'アソシエーションのテスト' do
-    context 'Userモデルとの関係' do
-      it 'N:1となっている' do
-        expect(Book.reflect_on_association(:user).macro).to eq :belongs_to
       end
     end
   end
